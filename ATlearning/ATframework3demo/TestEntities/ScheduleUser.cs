f678c98dacce48bc1d45912799169e2f2e1b5981
Kharymov Aleksey <t4woker@gmail.com>
2024-04-28 15:46:31 +0200

    add test entities: user, teacher

---
+++ ATlearning/ATframework3demo/TestEntities/ScheduleUser.cs
@@ -0,0 +1,40 @@
+﻿using System.Data;
+
+namespace ATframework3demo.TestEntities
+{
+    public class ScheduleUser
+    {
+        public string login { get; set; }
+
+        public string password { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string email { get; set; }
+
+        protected UserRole role { get; set; }
+
+        public string GetRoleName()
+        {
+            switch (this.role)
+            {
+                case UserRole.Teacher:
+                    return "Преподаватель";
+                case UserRole.Student:
+                    return "Студент";
+                case UserRole.Admin:
+                    return "Администратор";
+                default:
+                    return "";
+            }
+        }
+    }
+    public enum UserRole 
+    {
+        Teacher,
+        Student,
+        Admin
+    };
+}
---
+++ ATlearning/ATframework3demo/TestEntities/Teacher.cs
@@ -0,0 +1,14 @@
+﻿using atFrameWork2.TestEntities;
+using System.Collections.ObjectModel;
+
+namespace ATframework3demo.TestEntities
+{
+    public class Teacher : ScheduleUser
+    {
+        public Teacher()
+        {
+            role = UserRole.Teacher;
+        }
+        public List<string> subjects { get; set; }
+    }
+}
