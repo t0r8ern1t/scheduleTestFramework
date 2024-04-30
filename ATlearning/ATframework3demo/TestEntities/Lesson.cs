b13b18c3ec796dc27982f2991607d39978ec5a88
Kharymov Aleksey <t4woker@gmail.com>
2024-04-30 16:49:51 +0200

    rename `Class` class to `Lesson`, delete weekday enum

---
+++ ATlearning/ATframework3demo/TestEntities/Lesson.cs
@@ -0,0 +1,32 @@
+﻿namespace ATframework3demo.TestEntities
+{
+    public class Lesson
+    {
+        public Lesson(DayOfWeek dayOfWeek, int number, Subject subject, Audience audience, Teacher teacher, Groupe groupe)
+        {
+            this.dayOfWeek = dayOfWeek;
+            this.number = number;
+            this.subject = subject;
+            this.audience = audience;
+            this.teacher = teacher;
+            this.groupe = groupe;
+        }
+
+        public DayOfWeek dayOfWeek { get; set; }
+
+        public int number { get; set; }
+
+        public Subject subject { get; set; }
+
+        public Teacher teacher { get; set; }
+
+        public Audience audience { get; set; }
+
+        public Groupe groupe { get; set; }
+
+        public int GetNumberDayOfWeek()
+        {
+            return (int)dayOfWeek + 1;
+        }
+    }
+}
